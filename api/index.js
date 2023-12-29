49264b86e49b3acd81f94dab2bba98903a515c3b
Rishi47Sharma <rishigolu321@gmail.com>
2023-12-29 15:39:46 +0530

    client-side validations added

--- api/index.js
+++ api/index.js
@@ -1,41 +1,41 @@
-const express =require('express')
-const dotEnv= require('dotenv')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const User = require('./model/User')
+const express = require("express");
+const dotEnv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const User = require("./model/User");
 
 dotEnv.config();
 console.log(process.env.MONGO_URL);
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(
-    cors({
-        
-        origin:process.env.CLIENT_URL,
-    })
-)
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 
-const PORT =4000;
+const PORT = 4000;
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.post('/api/register',async(req,res)=>{
-    
-      try {
-        const{username,email,number,address}=req.body;
-     await User.create({username,email,number,address})
-     res.send();
-      } catch (error) {
-        res.send();
-      }
+app.post("/api/register", async (req, res) => {
+  try {
+    const { username, email, number, address } = req.body;
 
-    
+    await User.create({ username, email, number, address });
 
-} )
+    res.status(200).json({ message: "Request acknowledged successfully" });
+  } catch (error) {
+    console.error("Error processing the request:", error);
 
+    // If there's an error, send an HTTP 500 Internal Server Error response with an error message
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-
-app.listen(PORT,()=>{
-    console.log("server is running")
-})
-
+app.listen(PORT, () => {
+  console.log("server is running");
+});
